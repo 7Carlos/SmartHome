55c0dbfd827afae33a4129a9fa89643f08c9d8d3
Victoria <martinez15zz@live.com.mx>
2019-07-21 13:41:41 -0500

    se agregaron los modulos de boostrap

--- Ionic_App/SmartHome/src/app/alarmas/alarmas.page.ts
+++ Ionic_App/SmartHome/src/app/alarmas/alarmas.page.ts
@@ -1,5 +1,5 @@
 import { Component, OnInit } from '@angular/core';
-import { HTTP } from '@ionic-native/http/ngx';
+//import { HTTP } from '@ionic-native/http/ngx';
 
 @Component({
   selector: 'app-alarmas',
@@ -8,18 +8,8 @@ import { HTTP } from '@ionic-native/http/ngx';
 })
 export class AlarmasPage implements OnInit {
 
-  constructor(private http: HTTP) {
-    this.http.post('http://10.3.101.1/luces.html/', {}, {})
-    .then(data => {
-      console.log(data.status);
-      console.log(data.data); // data received by server
-      console.log(data.headers);
-    })
-    .catch(error => {
-      console.log(error.status);
-      console.log(error.error); // error message as string
-      console.log(error.headers);
-    });
+  constructor() {
+    
   }
 
 
